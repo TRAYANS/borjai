ad1ffe1a1f9b63d52a8ea3321594c743dc069d72
borjaquintero <borjabuzquin@gmail.com>
2026-08-24 00:50:59 +0200

    chore: bump app version to v1.3

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 export const APP_NAME = "BorjaAI";
-export const APP_VERSION = "V.1.2";
+export const APP_VERSION = "V.1.3";
 
 export function appSignature() {
   return APP_NAME + " · " + APP_VERSION;
