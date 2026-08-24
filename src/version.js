4a9a3bb01ddd9fb4156062b9709d053a47b2c847
borjaquintero <borjabuzquin@gmail.com>
2026-08-25 00:03:44 +0200

    chore(v1.4.1): mark audited stable release

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 export const APP_NAME = "BorjaAI";
-export const APP_VERSION = "V.1.4";
+export const APP_VERSION = "V.1.4.1";
 
 export function appSignature() {
   return APP_NAME + " · " + APP_VERSION;
