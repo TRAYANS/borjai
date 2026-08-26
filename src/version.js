6bcf9c2728c0ba3825487e26b565b7646ff71ada
borjaquintero <borjabuzquin@gmail.com>
2026-08-26 15:33:58 +0200

    chore(v1.6.1): expose current application version

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 export const APP_NAME = "BorjaAI";
-export const APP_VERSION = "V.1.6.0";
+export const APP_VERSION = "V.1.6.1";
 
 export function appSignature() {
   return APP_NAME + " · " + APP_VERSION;
