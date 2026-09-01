e86ab7fbf884f20cd0503fb1b58ac81ffcc623a2
borjaquintero <borjabuzquin@gmail.com>
2026-09-02 00:02:58 +0200

    Bump Gastos visual layer cache version

--- src/expenses-vision.js
+++ src/expenses-vision.js
@@ -4,6 +4,6 @@
   const link = document.createElement('link');
   link.id = 'borjai-expenses-vision-css';
   link.rel = 'stylesheet';
-  link.href = 'src/expenses-vision.css?v=1.8.7';
+  link.href = 'src/expenses-vision.css?v=1.8.8';
   document.head.appendChild(link);
 })();
