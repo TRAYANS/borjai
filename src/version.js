afd7acfaa6ae141d3dd51e2b16e42ca6477d36be
borjaquintero <borjabuzquin@gmail.com>
2026-09-02 13:44:39 +0200

    chore: set Borjai app version to 2.0.1

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 export const APP_NAME = "BorjaAI";
-export const APP_VERSION = "V2.0.0";
+export const APP_VERSION = "V2.0.1";
 
 export function appSignature() {
   return APP_NAME + " · " + APP_VERSION;
